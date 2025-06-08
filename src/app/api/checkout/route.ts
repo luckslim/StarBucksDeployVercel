87ebdec63c554947802c9c29a1ca73c564fd604f
lucaslima <lucaslima78@hotmail.com>
2025-06-08 08:31:02 -0300

    up

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -5,14 +5,14 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const cart = body.cart;
 
-  const line_items = cart.map((item:any) => ({
+  const line_items = cart.map((item: any) => ({
     price: item.idStripe,
     quantity: item.quantity,
   }));
 
   const checkoutSession = await stripe.checkout.sessions.create({
-    success_url: `${process.env.NEXT_URL}/sucesso?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${process.env.NEXT_URL}/`,
+    success_url: `https://star-bucks-deploy-vercel.vercel.app/sucesso?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `https://star-bucks-deploy-vercel.vercel.app`,
     mode: "payment",
     line_items,
   });
